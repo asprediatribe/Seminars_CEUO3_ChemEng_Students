511c812241920b30e7199249f1c5dc3c531e6beb
domoUOP3 <148894106+domoUOP3@users.noreply.github.com>
2024-01-17 17:27:52 +0000

    Update databankUOP3.py

--- databankUOP3.py
+++ databankUOP3.py
@@ -8,15 +8,16 @@
 # 3) "Mw" molecular weight
 # 4) "wac" acentric factor
 #def thermodynamic_constants(compounds):
+# properties is the name of the dictionary 
 properties = {
         "compound1"  : {"antoine" : [, , ], "critical": [, , ], "Mw": , "wac": },
         "compound2"  : {"antoine" : [, , ], "critical": [, , ], "Mw": , "wac": }
             }
 
 
-wace        = properties["compound"]["wac"]
-cts_antoine = properties["compound"]["antoine"][1]
-critics     = properties["compound"]["critical"][1]
+wace        = properties["compound"]["wac"]         # Requesting the  acentric factor, "wac", for the desired "compound"
+cts_antoine = properties["compound"]["antoine"][1]  # Requesting the  Antoine constant    (A,B, or C)    for the specified "compound",    given the position  in the array "antoine" (A=0, B=1, or C=2), 
+critics     = properties["compound"]["critical"][1] # Requesting the  critical properties (Tc,Pc,or Vc)  for the specified "compound",    given the position  in the array "critical" (Tc=0, Pc=1, or Vc=2), 
 
-print(wace , cts_antoine, critics)
-#return(cts_antoine,critics,wace)
+print(wace , cts_antoine, critics) 
+#return(cts_antoine,critics, wace)
